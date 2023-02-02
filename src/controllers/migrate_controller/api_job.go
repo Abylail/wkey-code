78c760975626eb776ba1388bc56bd6d0c4e05278
lazy_owl <slimsly11@gmail.com>
2023-02-02 23:22:12 +0600

    run jobs at specific time

--- src/controllers/migrate_controller/api_job.go
+++ src/controllers/migrate_controller/api_job.go
@@ -1,9 +1,15 @@
 package migrate_controller
 
+import (
+	"github.com/lowl11/lazylog/layers"
+	"wkey-core/src/definition"
+)
+
 /*
 	CategoriesJob обертка для _categories
 */
 func (controller *Controller) CategoriesJob() error {
+	definition.Logger.Info("Categories migrate job started...", layers.Job)
 	return controller._categories()
 }
 
@@ -11,5 +17,6 @@ func (controller *Controller) CategoriesJob() error {
 	ProductsJob обертка для _products
 */
 func (controller *Controller) ProductsJob() error {
+	definition.Logger.Info("Products migrate Job started...", layers.Job)
 	return controller._products()
 }
--- src/job/job_server/internal.go
+++ src/job/job_server/internal.go
@@ -9,23 +9,23 @@ func (server *Server) tasks() {
 	logger := definition.Logger
 	_ = logger
 
-	//if _, err := server.scheduler.Cron("30 1 * * *").Do(server.migrate.CategoriesJob); err != nil {
-	//	logger.Error(err, "Call categories migration error", layers.Job)
-	//}
-
-	// !!! uncomment next lines for localhost
-	if _, err := server.scheduler.Every(1).Hours().Do(server.migrate.CategoriesJob); err != nil {
+	if _, err := server.scheduler.Cron("30 1 * * *").Do(server.migrate.CategoriesJob); err != nil {
 		logger.Error(err, "Call categories migration error", layers.Job)
 	}
 
-	//if _, err := server.scheduler.Cron("35 1 * * *").Do(server.migrate.ProductsJob()); err != nil {
-	//	logger.Error(err, "Call products migration error", layers.Job)
+	// !!! uncomment next lines for localhost
+	//if _, err := server.scheduler.Every(1).Hours().Do(server.migrate.CategoriesJob); err != nil {
+	//	logger.Error(err, "Call categories migration error", layers.Job)
 	//}
 
-	// !!! uncomment next lines for localhost
-	if _, err := server.scheduler.Every(1).Hours().Do(server.migrate.ProductsJob); err != nil {
+	if _, err := server.scheduler.Cron("35 1 * * *").Do(server.migrate.ProductsJob); err != nil {
 		logger.Error(err, "Call products migration error", layers.Job)
 	}
+
+	// !!! uncomment next lines for localhost
+	//if _, err := server.scheduler.Every(1).Hours().Do(server.migrate.ProductsJob); err != nil {
+	//	logger.Error(err, "Call products migration error", layers.Job)
+	//}
 }
 
 func (server *Server) unauthorizedFeed() error {
