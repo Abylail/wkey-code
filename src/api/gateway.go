b3cb9a8cc08921a7f21a82eaeef0d611d936f13e
lazy_owl <slimsly11@gmail.com>
2023-02-08 17:29:04 +0600

    add auth gateway routes

--- src/api/gateway.go
+++ src/api/gateway.go
@@ -7,6 +7,21 @@ import (
 )
 
 func setGateway(router *gateway.Client) {
+	setAuth(router)
+	setStock(router)
+}
+
+func setAuth(router *gateway.Client) {
+	config := definition.Config
+
+	authClientRoute := lazyway.Route("/api/v1/auth").SetHosts(config.Hosts...).SetPort(":8084")
+	authAdminRoute := lazyway.Route("/admin/api/v1/auth").SetHosts(config.Hosts...).SetPort(":8084")
+
+	router.Route(authClientRoute)
+	router.Route(authAdminRoute)
+}
+
+func setStock(router *gateway.Client) {
 	config := definition.Config
 
 	stockClientRoute := lazyway.Route("/api/v1/stock").SetHosts(config.Hosts...).SetPort(":8083")
